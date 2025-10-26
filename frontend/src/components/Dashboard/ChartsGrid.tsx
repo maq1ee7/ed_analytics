bc7dfed5b6e30c1ee8cddc361c225d4b20c396a2
maq <maq1ee7@gmail.com>
2025-10-26 14:54:34 +0300

    IT-7 remove historgram

--- frontend/src/components/Dashboard/ChartsGrid.tsx
+++ frontend/src/components/Dashboard/ChartsGrid.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ExponentialChart from '../charts/ExponentialChart';
-import HistogramChart from '../charts/HistogramChart';
 import RussiaInteractiveMap from '../charts/RussiaInteractiveMap';
 import RadarChart from '../charts/RadarChart';
 import FederalSharesChart from '../charts/FederalSharesChart';
@@ -8,14 +7,9 @@ import FederalSharesChart from '../charts/FederalSharesChart';
 const ChartsGrid: React.FC = () => {
   return (
     <div className="space-y-6">
-      {/* Первый ряд - два основных графика */}
-      <div className="grid grid-cols-1 xl:grid-cols-2 gap-6">
-        <div className="w-full min-h-0">
-          <ExponentialChart />
-        </div>
-        <div className="w-full min-h-0">
-          <HistogramChart />
-        </div>
+      {/* Первый ряд - основной график */}
+      <div className="w-full min-h-0">
+        <ExponentialChart />
       </div>
 
       {/* Второй ряд - региональные показатели */}
